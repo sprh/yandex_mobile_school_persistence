2195fde31b1fc798a456c8e95917978a17230c13
Sofia <sktimokhina@yandex-team.ru>
2023-06-11 18:46:49 +0300

    File example

--- lib/managers/persistence_manager.dart
+++ lib/managers/persistence_manager.dart
@@ -1,9 +1,37 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 import 'package:yandex_mobile_school_persistence/models/github_profile.dart';
 
 class PersistenceManager {
-  Future<List<GithubProfile>> getSearchHistory() async {
-    return [];
+  Future<File> get _file async {
+    final appDir = await getApplicationDocumentsDirectory();
+    final path = "${appDir.path}/git_2.txt";
+    return File(path);
   }
 
-  Future<void> saveProfile({required GithubProfile profile}) async {}
+  Future<List<GithubProfile>> getSearchHistory() async {
+    final file = await _file;
+    if (!await file.exists()) {
+      return [];
+    }
+
+    final lines = await file.readAsLines();
+    return lines.map(
+      (line) {
+        final json = jsonDecode(line) as Map<String, dynamic>;
+        return GithubProfile.fromJson(json);
+      },
+    ).toList();
+  }
+
+  Future<void> saveProfile({required GithubProfile profile}) async {
+    final file = await _file;
+    final ioSink = file.openWrite(mode: FileMode.append);
+    final jsonString = jsonEncode(profile.toJson());
+    ioSink.writeln(jsonString);
+    await ioSink.flush();
+    await ioSink.close();
+  }
 }
